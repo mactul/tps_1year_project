f6a92417a2a2d1b7498060aeadbd59e0431e3dd6
mactul <mactulgames@gmail.com>
2023-11-28 11:51:00 +0100

    add xmake.lua

--- src/main.c
+++ src/main.c
@@ -3,6 +3,10 @@
 
 int main()
 {
+    SA_init();
+
     SA_DynamicArray* films = SA_dynarray_create(Film);
     read_movie_file(films, "download/training_set/mv_0000001.txt");
+
+    SA_destroy();
 }
---
+++ xmake.lua
@@ -0,0 +1,19 @@
+add_rules("mode.debug", "mode.release")
+
+set_policy("build.warning", true)
+set_warnings("all", "extra")
+
+
+rule("flags_extras")
+    if is_mode("debug") then
+        on_config(function(target)
+            target:add("defines", "DEBUG")
+        end)
+    end
+
+target("film_stats")
+    add_includedirs("./", {public = true})
+    add_files("**.c")
+    add_rules("flags_extras")
+    add_links("sa")
+    add_cxflags("-pedantic", "-O3")
