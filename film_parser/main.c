f29d02204b8c1e067c0c8036655ce8e881ecc222
mactul <mactulgames@gmail.com>
2023-11-29 17:25:21 +0100

    change structure, there is now 2 executable files

---
+++ film_parser/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <SA/SA.h>
+#include "src/io/parser_txt.h"
+#include "src/io/parser_bin.h"
+#include "src/io/writer_bin.h"
+#include "src/arg-handler.h"
+
+#define MAX_OUT_FOLDER_PATH 256
+
+int main(int argc, char* argv[])
+{
+    SA_DynamicArray* films = NULL;
+    char out_folder_path[MAX_OUT_FOLDER_PATH] = "out/data.bin";
+
+    SA_init();
+
+    films = SA_dynarray_create(Film);
+    if(films == NULL)
+    {
+        goto EXIT_LBL;
+    }
+
+    for (int i = 1; i < argc; i++)
+    {
+        if(SA_strcmp(argv[i], "-o") == 0)
+        {
+            i++;
+            if(i < argc)
+            {
+                SA_strncpy(out_folder_path, argv[i], MAX_OUT_FOLDER_PATH);
+            }
+        }
+        else if(read_movie_file(films, argv[i]) != 0)
+        {
+            goto EXIT_LBL;
+        }
+    }
+    
+    write_films(out_folder_path, films);
+
+EXIT_LBL:
+    films_list_free(&films);
+
+    SA_destroy();
+}
---
+++ film_stats/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <SA/SA.h>
+#include "src/io/parser_txt.h"
+#include "src/io/parser_bin.h"
+#include "src/io/writer_bin.h"
+#include "src/arg-handler.h"
+
+#define MAX_OUT_FOLDER_PATH 256
+
+
+int main(int argc, char* argv[])
+{
+    SA_DynamicArray* films = NULL;
+    char out_folder_path[MAX_OUT_FOLDER_PATH] = "out/data.bin";
+    Arguments args_structure;
+    int index_remaining;
+
+    SA_init();
+
+
+    if (!parse_args(argc, argv, &args_structure, &index_remaining))
+    {
+        goto EXIT_LBL;
+    }
+
+    if(index_remaining < argc)
+    {
+        SA_strcpy(out_folder_path, argv[index_remaining]);
+    }
+
+    films = read_all_films(out_folder_path);
+    if (films == NULL)
+    {
+        goto EXIT_LBL;
+    }
+
+EXIT_LBL:
+    films_list_free(&films);
+
+    SA_destroy();
+}
