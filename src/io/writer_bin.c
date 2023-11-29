38871f6c44f233368c4c5abc41c355214022a44a
Valentin FOULON <valentin.foulon@etu.unistra.fr>
2023-11-29 09:47:12 +0100

    Ajout d'un mkdir récursif dans writer_bin

--- src/io/writer_bin.c
+++ src/io/writer_bin.c
@@ -1,10 +1,70 @@
 #include "writer_bin.h"
 
+void make_path(char* out_folder)
+{
+    char* out_copy = malloc((strlen(out_folder) + 1) * sizeof(char));
+    strcpy(out_copy, out_folder);
+
+    long last_slash = -1;
+
+    for (size_t i = 0; i < strlen(out_folder); i++)
+    {
+        if (out_folder[i] == '/')
+        {
+            last_slash = i;
+        }
+    }
+
+    if (last_slash == -1)
+    {
+        struct stat st = {0};
+        if (stat(out_copy, &st) == -1)
+        {
+            mkdir(out_copy, 0755);
+        }
+        free(out_copy);
+        return;
+    }
+    
+    out_folder[last_slash] = '\0';
+
+    make_path(out_folder);
+
+    struct stat st = {0};
+    if (stat(out_copy, &st) == -1)
+    {
+        mkdir(out_copy, 0755);
+    }
+
+    free(out_copy);
+}
+
 int write_films(const char* out_filename, SA_DynamicArray* films)
 {
+    char* out_folder = malloc((strlen(out_filename) + 1) * sizeof(char));
+    strcpy(out_folder, out_filename);
+
+    long last_slash = -1;
+
+    for (size_t i = 0; i < strlen(out_filename); i++)
+    {
+        if (out_folder[i] == '/')
+        {
+            last_slash = i;
+        }
+    }
+
+    if (last_slash != -1)
+    {
+        out_folder[last_slash] = '\0';
+        make_path(out_folder);
+    }
+
+    free(out_folder);
+
     if (films == NULL)
     {
-        return 2;
+        return 6;
     }
 
     int error_code = 0;
@@ -43,14 +103,14 @@ int write_films(const char* out_filename, SA_DynamicArray* films)
             Rating r = SA_dynarray_get(Rating, f.ratings, j);
             if (fwrite(&r, sizeof(Rating), 1, file) <= 0)
             {
-                error_code = 2;
+                error_code = 4;
                 goto QUIT;
             }
         }
         fseek(file, sizeof(uint64_t) + i * sizeof(Film) + 16, SEEK_SET);
         if (fwrite(&pos, sizeof(long), 1, file) <= 0)
         {
-            error_code = 4;
+            error_code = 5;
             goto QUIT;
         }
     }
--- src/io/writer_bin.h
+++ src/io/writer_bin.h
@@ -3,6 +3,11 @@
 
 #include <SA/memory/dynamic_array.h>
 #include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
 #include "src/data_structs.h"
 
 int write_films(const char* out_filename, SA_DynamicArray* films);
